073320a9173ee69fdd09984557db9bd2a0f8038a
Zia999 <92579255+Zia999@users.noreply.github.com>
2023-01-31 17:56:20 +0000

    game version 1

--- Cat/script.js
+++ Cat/script.js
@@ -10,6 +10,7 @@ async function fetchCatInformation() {
   const breedNames = randomBreeds.map((breed) => breed.name);
 
   createBreedList(breedNames);
+  createBreedBoxes(randomBreeds);
 
   //   for (const breed of randomBreeds) {
   //     console.log(breed.name);
@@ -21,6 +22,8 @@ async function fetchCatInformation() {
   //     console.log("Cat Breed Temperament: ", breed.temperament);
   //     console.log("---");
   //   }
+
+  selectCat();
 }
 
 fetchCatInformation();
@@ -28,16 +31,130 @@ fetchCatInformation();
 /** TASK2: create breed list **/
 function createBreedList(breedList) {
   document.getElementById("breed").innerHTML = `
-  <label>Your cat is</label>
-  <select>
-  <option>Cat breed</option>
+ 
   ${breedList.map((name) => `<option>${name}</option>`).join("")}
-</select>
+
   `;
 }
 
-/** TASK3: push info for each cat into div box **/
+/** TASK3: push in  fo for each cat into div box **/
+function createBreedBoxes(breeds) {
+  const breedContainer = document.getElementById("breed-container");
+  const uniqueTemperaments = Array.from(
+    new Set(
+      breeds.flatMap((b) =>
+        b.temperament.split(", ").map((word) => word.replace(/,$/, ""))
+      )
+    )
+  );
+  console.log(uniqueTemperaments);
 
-/**TASK4: temperament make to array without duplication and push it to the list **/
+  for (const breed of breeds) {
+    const breedDiv = document.createElement("div");
+    breedDiv.className = "cat-info";
+
+    const breedName = document.createElement("h2");
+    breedName.className = "cat-name";
+    breedName.textContent = breed.name;
+    breedDiv.appendChild(breedName);
+
+    const breedTemperament = document.createElement("p");
+    breedTemperament.className = "cat-temperament";
+    breedTemperament.textContent = breed.temperament;
+
+    breedDiv.appendChild(breedTemperament);
+
+    breedContainer.appendChild(breedDiv);
+  }
+
+  /**TASK4: temperament make to array without duplication and push it to the list **/ //
+  const checkList = document.getElementById("select-feature");
+  for (const temperament of uniqueTemperaments) {
+    const option = document.createElement("option");
+    option.textContent = temperament;
+    checkList.appendChild(option);
+  }
+}
 
 /**TASK5: TMR: game theory **/
+let selectedCat = null;
+
+// Randomly select one cat from the breed container div
+function selectCat() {
+  const breedContainer = document.getElementById("breed-container");
+  const cats = breedContainer.getElementsByClassName("cat-info");
+  selectedCat = cats[Math.floor(Math.random() * cats.length)];
+  console.log("Selected cat:", selectedCat);
+}
+
+// Listen to the click event on the "ask" button
+document.getElementById("btn-ask").addEventListener("click", () => {
+  const selectedTemperament = document.querySelector(
+    "select.select-feature"
+  ).value;
+
+  console.log("Selected temperament:", selectedTemperament);
+
+  // Cross out the cats which don't have the selected temperament
+  const catInfoCards = document.querySelectorAll(".cat-info");
+
+  //   for (const catInfoCard of catInfoCards) {
+  //     const catTemperaments =
+  //       catInfoCard.querySelector("p.cat-temperament").textContent;
+  //     if (catTemperaments.includes(selectedTemperament)) {
+  //       if (!catInfoCard.classList.contains("crossed-out")) {
+  //         catInfoCard.classList.add("crossed-out");
+  //       }
+  //     }
+  //   }
+
+  if (
+    selectedCat
+      .querySelector(".cat-temperament")
+      .textContent.includes(selectedTemperament)
+  ) {
+    alert("YES");
+
+    catInfoCards.forEach((catInfoCard) => {
+      if (
+        !catInfoCard
+          .querySelector(".cat-temperament")
+          .textContent.includes(selectedTemperament)
+      ) {
+        if (!catInfoCard.classList.contains("crossed-out")) {
+          catInfoCard.classList.add("crossed-out");
+        }
+      }
+    });
+  } else {
+    alert("Nope");
+    catInfoCards.forEach((catInfoCard) => {
+      if (
+        catInfoCard
+          .querySelector(".cat-temperament")
+          .textContent.includes(selectedTemperament)
+      ) {
+        if (!catInfoCard.classList.contains("crossed-out")) {
+          catInfoCard.classList.add("crossed-out");
+        }
+      }
+    });
+  }
+});
+
+//Submit final guess
+const btnGuess = document.querySelector(".btn-guess");
+
+btnGuess.addEventListener("click", () => {
+  const selectedBreed = document.querySelector("#breed").value;
+
+  console.log("selectedBreed:", selectedBreed);
+
+  const selectedCatName = selectedCat.querySelector(".cat-name").textContent;
+
+  if (selectedBreed === selectedCatName) {
+    alert(`Congrats! Correct Guess! The Cat is ${selectedCatName}`);
+  } else {
+    alert("Oops, wrong guess:(");
+  }
+});
